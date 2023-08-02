9c0f730c26d6de6b6023fef430c07cd0dcda07a0
Salim <salimhetham@gmail.com>
2023-08-02 18:28:28 +0300

    completed

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    fetch('https://anapioficeandfire.com/api/books')
+        .then((res) => res.json())
+        .then((books) => { renderBooks(books) })
+        .catch((error) => (error))
+
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const main = document.querySelector('main');
+    books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+    fetchBooks();
 });
